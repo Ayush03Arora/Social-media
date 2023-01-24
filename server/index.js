d8b930863564e3064de0beb31f5be7f63cd146e5
Ayush <aayusharora4444@gmail.com>
2023-01-25 01:27:50 +0530

    backend complete

--- server/index.js
+++ server/index.js
@@ -9,7 +9,14 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/user.js";
+import postRoutes from "./routes/posts.js"
 import {register} from "./controllers/auth.js";
+import {createPost} from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users,posts } from "./data/index.js";
 
 // Configurations(Middlewares)
 
@@ -35,18 +42,24 @@ const storage = multer.diskStorage({
 const upload = multer({storage});
 
 // Routes with files
-
 app.post("/auth/register",upload.single("picture"),register);
-
+app.post("/posts",verifyToken,upload.single("picture"),createPost);
 //Routes
 app.use("/auth",authRoutes);
+app.use("/users",userRoutes);
+app.use("/posts",postRoutes);
 // Mongoose
 
 const PORT = process.env.PORT || 6001;
+mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGO_URL ,{
     useNewUrlParser : true,
     useUnifiedTopology: true,
 }).then(()=> {
   app.listen(PORT,() => console.log(`Server Port: ${PORT}`));
+
+  // For adding one time then after comment it
+  // User.insertMany(users);
+  // Post.insertMany(posts);
 })
 .catch((error) => console.log(`${error} did not connect`));
