3b61a51c91fb11a2e930e158486ae7dfabfb6cc7
Ayush <aayusharora4444@gmail.com>
2023-02-10 19:13:19 +0530

    completed

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -3,6 +3,9 @@ import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidget";
 import MyPostWidget from 'scenes/widgets/MyPostWidget';
+import PostsWidget from "scenes/widgets/PostsWidget";
+import AdvertWidget from "scenes/widgets/AdvertWidget";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
 
 
 const HomePage =() =>{
@@ -26,9 +29,14 @@ const HomePage =() =>{
             flexBasis={isNonMobileScreens ? "42%" :undefined }
             mt={isNonMobileScreens ? undefined : "2rem"}>
             <MyPostWidget picturePath={picturePath} />
+            <PostsWidget userId={_id} />
             </Box>
             {isNonMobileScreens && (
-                <Box flexBasis="26%"></Box>
+                <Box flexBasis="26%">
+                <AdvertWidget />    
+                <Box m="2rem 0" />
+                <FriendListWidget userId={_id}/>
+                </Box>
             )}
 
             </Box>
